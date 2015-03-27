490460c02be586dab395723c57a5f2b173767ff2
Michal Minar <miminar@redhat.com>
2015-03-27 10:49:21 -0400

    Improved searching experience
    - Prepend indicies to search results.
    - Prefix remote name with registry name.
    - Shorten index name if it's not an ip address.
    - Added `--no-index` option to avoid listing the index name.
    - Changed sorting of entries when index is preserved:
    
      1. sort by `index_name`
      2. sort by `star_count`
      3. sort by `registry_name`
      4. sort by `name`
      5. sort by `description`
    
    - Changed sorting of entries when index is omitted:
    
      1. sort by `registry_name`
      2. sort by `star_count`
      3. sort by `name`
      4. sort by `description`
    
    Example (with just first two columns left):
    
        $ sudo docker search mongo
        INDEX       NAME                                                     DESCRIPTION
        docker.io   docker.io/mongo                                          MongoDB docu...
        [...]
        myregistry  docker.io/mongo                                          test
        [...]
        redhat.com  docker-registry.usersys.redhat.com/goldmann/eap-mongodb
        [...]
    
    Docker-DCO-1.1-Signed-off-by: Daniel Riek <riek@redhat.com>
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1448,8 +1448,9 @@ func (daemon *Daemon) AuthenticateToRegistry(authConfig *cliconfig.AuthConfig) (
 // term. authConfig is used to login.
 func (daemon *Daemon) SearchRegistryForImages(term string,
 	authConfig *cliconfig.AuthConfig,
-	headers map[string][]string) (*registry.SearchResults, error) {
-	return daemon.RegistryService.Search(term, authConfig, headers)
+	headers map[string][]string,
+	noIndex bool) ([]registry.SearchResultExt, error) {
+	return daemon.RegistryService.Search(term, authConfig, headers, noIndex)
 }
 
 // IsShuttingDown tells whether the daemon is shutting down or not
