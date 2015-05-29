e1f3f2fcdab039ed56d42ed29da84e67565f922f
boucher <rboucher@gmail.com>
2015-05-29 13:06:02 -0700

    Update checkpoint/restore support in the daemon.
    Docker-DCO-1.1-Signed-off-by: Ross Boucher <rboucher@gmail.com> (github: boucher)

--- daemon/container.go
+++ daemon/container.go
@@ -13,6 +13,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/docker/libcontainer"
 	"github.com/docker/libcontainer/label"
 
 	"github.com/Sirupsen/logrus"
@@ -248,7 +249,7 @@ func (container *Container) Start() (err error) {
 	if err := container.Mount(); err != nil {
 		return err
 	}
-	if err := container.initializeNetworking(); err != nil {
+	if err := container.initializeNetworking(false); err != nil {
 		return err
 	}
 	container.verifyDaemonSettings()
@@ -332,12 +333,11 @@ func (container *Container) isNetworkAllocated() bool {
 	return container.NetworkSettings.IPAddress != ""
 }
 
-
 // cleanup releases any network resources allocated to the container along with any rules
 // around how containers are linked together.  It also unmounts the container's root filesystem.
 func (container *Container) cleanup() {
 	if container.IsCheckpointed() {
-		log.CRDbg("not calling ReleaseNetwork() for checkpointed container %s", container.ID)
+		logrus.Debugf("not calling ReleaseNetwork() for checkpointed container %s", container.ID)
 	} else {
 		container.ReleaseNetwork()
 	}
@@ -581,7 +581,6 @@ func validateID(id string) error {
 	return nil
 }
 
-
 func (container *Container) Checkpoint(opts *libcontainer.CriuOpts) error {
 	if err := container.daemon.Checkpoint(container, opts); err != nil {
 		return err
@@ -593,6 +592,52 @@ func (container *Container) Checkpoint(opts *libcontainer.CriuOpts) error {
 	return nil
 }
 
+func (container *Container) Restore(opts *libcontainer.CriuOpts, forceRestore bool) error {
+	var err error
+	container.Lock()
+	defer container.Unlock()
+
+	defer func() {
+		if err != nil {
+			container.setError(err)
+			// if no one else has set it, make sure we don't leave it at zero
+			if container.ExitCode == 0 {
+				container.ExitCode = 128
+			}
+			container.toDisk()
+			container.cleanup()
+		}
+	}()
+
+	if err := container.Mount(); err != nil {
+		return err
+	}
+	if err = container.initializeNetworking(true); err != nil {
+		return err
+	}
+	container.verifyDaemonSettings()
+
+	linkedEnv, err := container.setupLinkedContainers()
+	if err != nil {
+		return err
+	}
+	if err = container.setupWorkingDirectory(); err != nil {
+		return err
+	}
+
+	env := container.createDaemonEnvironment(linkedEnv)
+	if err = populateCommand(container, env); err != nil {
+		return err
+	}
+
+	mounts, err := container.setupMounts()
+	if err != nil {
+		return err
+	}
+
+	container.command.Mounts = mounts
+	return container.waitForRestore(opts, forceRestore)
+}
 
 func (container *Container) Copy(resource string) (io.ReadCloser, error) {
 	container.Lock()
@@ -653,41 +698,6 @@ func (container *Container) Copy(resource string) (io.ReadCloser, error) {
 			return err
 		}),
 		nil
-}
-
-func (container *Container) Checkpoint() error {
-	return container.daemon.Checkpoint(container)
-}
-
-func (container *Container) Restore() error {
-	var err error
-
-	container.Lock()
-	defer container.Unlock()
-
-	defer func() {
-		if err != nil {
-			container.cleanup()
-		}
-	}()
-
-	if err = container.initializeNetworking(); err != nil {
-		return err
-	}
-
-	linkedEnv, err := container.setupLinkedContainers()
-	if err != nil {
-		return err
-	}
-	if err = container.setupWorkingDirectory(); err != nil {
-		return err
-	}
-	env := container.createDaemonEnvironment(linkedEnv)
-	if err = populateCommandRestore(container, env); err != nil {
-		return err
-	}
-
-	return container.waitForRestore()
 }
 
 // Returns true if the container exposes a certain port
@@ -776,10 +786,7 @@ func (container *Container) waitForStart() error {
 	return nil
 }
 
-// Like waitForStart() but for restoring a container.
-//
-// XXX Does RestartPolicy apply here?
-func (container *Container) waitForRestore() error {
+func (container *Container) waitForRestore(opts *libcontainer.CriuOpts, forceRestore bool) error {
 	container.monitor = newContainerMonitor(container, container.hostConfig.RestartPolicy)
 
 	// After calling promise.Go() we'll have two goroutines:
@@ -792,7 +799,7 @@ func (container *Container) waitForRestore() error {
 		if container.ExitCode != 0 {
 			return fmt.Errorf("restore process failed")
 		}
-	case err := <-promise.Go(container.monitor.Restore):
+	case err := <-promise.Go(func() error { return container.monitor.Restore(opts, forceRestore) }):
 		return err
 	}
 
@@ -1003,6 +1010,7 @@ func attach(streamConfig *StreamConfig, openStdin, stdinOnce, tty bool, stdin io
 			_, err = copyEscapable(cStdin, stdin)
 		} else {
 			_, err = io.Copy(cStdin, stdin)
+
 		}
 		if err == io.ErrClosedPipe {
 			err = nil
