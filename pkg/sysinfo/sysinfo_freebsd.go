c2f04ec4178e38dab5632fbaf06825d4ac0bd5d1
Antonio Murdaca <runcom@linux.com>
2015-09-10 19:47:12 +0200

    wip

--- pkg/sysinfo/sysinfo_freebsd.go
+++ pkg/sysinfo/sysinfo_freebsd.go
@@ -1,5 +1,13 @@
 package sysinfo
 
+// SysInfo holds fields specific to the freebsd implementation. See
+// CommonSysInfo for standard fields common to all platforms.
+type SysInfo struct {
+	CommonSysInfo
+
+	// Fields below here are platform specific.
+}
+
 // New returns an empty SysInfo for freebsd for now.
 func New(quiet bool) *SysInfo {
 	sysInfo := &SysInfo{}
--- pkg/sysinfo/sysinfo_windows.go
+++ pkg/sysinfo/sysinfo_windows.go
@@ -1,5 +1,13 @@
 package sysinfo
 
+// SysInfo holds fields specific to the Windows implementation. See
+// CommonSysInfo for standard fields common to all platforms.
+type SysInfo struct {
+	CommonSysInfo
+
+	// Fields below here are platform specific.
+}
+
 // New returns an empty SysInfo for windows for now.
 func New(quiet bool) *SysInfo {
 	sysInfo := &SysInfo{}
